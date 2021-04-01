95c04c6e0df196eec83c168719c16fe5ea6aa27b
Mauricio Lima <mauricio@codebit.com.br>
2021-04-01 15:52:48 -0300

    feat: configure axios and turn Main as class component

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,24 +1,78 @@
-import React from 'react';
-import { FaGithubAlt, FaPlus } from 'react-icons/fa';
-import { Container, Form, SubmitButton } from './styles';
+import React, { Component } from 'react';
+import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
+import api from '../../services/api';
+import { Container, Form, SubmitButton, List } from './styles';
 
-function Main() {
-  return (
-    <Container>
-      <h1>
-        <FaGithubAlt />
-        Repositórios
-      </h1>
+class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newRepo: '',
+      repositories: [],
+      loading: false,
+    };
+  }
 
-      <Form onSubmit={() => {}}>
-        <input type='text' placeholder='Adicionar Repositório' />
+  handleInputChage = (e) => {
+    this.setState({ newRepo: e.target.value });
+  };
 
-        <SubmitButton disable={false}>
-          <FaPlus color='#fff' size={14} />
-        </SubmitButton>
-      </Form>
-    </Container>
-  );
+  handleSubmit = async (e) => {
+    e.preventDefault();
+    this.setState({ loading: true });
+
+    const { newRepo, repositories } = this.state;
+    const response = await api.get(`/repos/${newRepo}`);
+
+    const data = {
+      name: response.data.full_name,
+    };
+
+    this.setState({
+      repositories: [...repositories, data],
+      newRepo: '',
+      loading: false,
+    });
+  };
+
+  render() {
+    const { newRepo, loading, repositories } = this.state;
+
+    return (
+      <Container>
+        <h1>
+          <FaGithubAlt />
+          Repositórios
+        </h1>
+
+        <Form onSubmit={this.handleSubmit}>
+          <input
+            type='text'
+            placeholder='Adicionar Repositório'
+            value={newRepo}
+            onChange={this.handleInputChage}
+          />
+
+          <SubmitButton loading={loading}>
+            {loading ? (
+              <FaSpinner color='#fff' size={14} />
+            ) : (
+              <FaPlus color='#fff' size={14} />
+            )}
+          </SubmitButton>
+        </Form>
+
+        <List>
+          {repositories.map((repo) => (
+            <li key={repo.name}>
+              <span>{repo.name}</span>
+              <a href='alow'>Detalhes</a>
+            </li>
+          ))}
+        </List>
+      </Container>
+    );
+  }
 }
 
 export default Main;
