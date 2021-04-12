b85054ba734551bc1c976c5993ca9463512fd554
Mauricio Lima <mauricio@codebit.com.br>
2021-04-12 20:36:34 -0300

    feat: finish Repository component

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import api from '../../services/api';
-import { Loading, Owner } from './styles';
+import { Loading, Owner, IssueList } from './styles';
 import Container from '../../components/Container';
 
 class Repository extends Component {
@@ -50,8 +50,6 @@ class Repository extends Component {
   render() {
     const { repository, issues, loading } = this.state;
 
-    console.log(issues);
-
     if (loading) {
       return <Loading>Carregando...</Loading>;
     }
@@ -64,6 +62,26 @@ class Repository extends Component {
           <h1>{repository.name}</h1>
           <p>{repository.description}</p>
         </Owner>
+
+        <IssueList>
+          {issues.map((issue) => (
+            <li key={String(issue.id)}>
+              <img
+                src={issue.user.avatar_url}
+                alt={`profile of ${issue.user.login}`}
+              />
+              <div>
+                <strong>
+                  <a href={issue.html_url}>{issue.title}</a>
+                  {issue.labels.map((label) => (
+                    <span key={String(label.id)}>{label.name}</span>
+                  ))}
+                </strong>
+                <p>{issue.user.login}</p>
+              </div>
+            </li>
+          ))}
+        </IssueList>
       </Container>
     );
   }
