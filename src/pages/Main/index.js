b0557cf7080a59a9a43e8414c688c996882c730c
Mauricio Lima <mauricio@codebit.com.br>
2021-04-13 20:45:55 -0300

    chore: use Main component as const
    instead of class

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,51 +1,40 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import { Form, Input, SubmitButton, List } from './styles';
 import api from '../../services/api';
 import Container from '../../components/Container';
 
-class Main extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      newRepo: '',
-      repositories: [],
-      loading: false,
-      formError: false,
-    };
-  }
+const Main = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [formError, setFormError] = useState(false);
 
-  // carregar os dados que estão no localStorage
-  componentDidMount() {
-    const repositories = localStorage.getItem('repositories');
+  useEffect(() => {
+    const persistedRepositories = localStorage.getItem('repositories');
 
-    if (repositories) {
-      this.setState({ repositories: JSON.parse(repositories) });
+    if (persistedRepositories) {
+      setRepositories(JSON.parse(persistedRepositories));
     }
-  }
+  }, []);
 
-  // salvar o estado repositories novamente no localStorage
-  componentDidUpdate(_, prevState) {
-    const { repositories } = this.state;
+  useEffect(() => {
+    localStorage.setItem('repositories', JSON.stringify(repositories));
+  }, [repositories]);
 
-    if (prevState.repositories !== repositories) {
-      localStorage.setItem('repositories', JSON.stringify(repositories));
-    }
-  }
-
-  handleInputChage = (e) => {
-    this.setState({ newRepo: e.target.value, formError: false });
+  const handleInputChange = (e) => {
+    setNewRepo(e.target.value);
+    setFormError(false);
   };
 
-  handleSubmit = async (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    this.setState({ loading: true });
-
-    const { newRepo, repositories } = this.state;
+    setLoading(true);
 
     if (!newRepo) {
-      this.setState({ formError: true, loading: false });
+      setFormError(true);
+      setLoading(false);
       return;
     }
 
@@ -54,67 +43,58 @@ class Main extends Component {
 
       const data = {
         name: response.data.full_name,
-        // link: `github.com${response.config.url}`,
       };
 
-      this.setState({
-        repositories: [...repositories, data],
-        newRepo: '',
-      });
+      setRepositories([...repositories, data]);
+      setNewRepo('');
     } catch (error) {
-      this.setState({ formError: true });
+      setFormError(true);
     } finally {
-      this.setState({ loading: false });
+      setLoading(false);
     }
   };
 
-  render() {
-    const { newRepo, loading, repositories, formError } = this.state;
+  return (
+    <Container>
+      <h1>
+        <FaGithubAlt />
+        Repositórios
+      </h1>
 
-    return (
-      <Container>
-        <h1>
-          <FaGithubAlt />
-          Repositórios
-        </h1>
+      <Form onSubmit={handleSubmit}>
+        <Input
+          type='text'
+          placeholder='Adicionar Repositório'
+          value={newRepo}
+          onChange={handleInputChange}
+          error={formError}
+        />
 
-        <Form onSubmit={this.handleSubmit}>
-          <Input
-            type='text'
-            placeholder='Adicionar Repositório'
-            value={newRepo}
-            onChange={this.handleInputChage}
-            error={formError}
-          />
+        <SubmitButton loading={loading ? 1 : undefined}>
+          {loading ? (
+            <FaSpinner color='#fff' size={14} />
+          ) : (
+            <FaPlus color='#fff' size={14} />
+          )}
+        </SubmitButton>
+      </Form>
+      {formError && <span style={{ color: 'red' }}>Repositório inválido</span>}
 
-          <SubmitButton loading={loading}>
-            {loading ? (
-              <FaSpinner color='#fff' size={14} />
-            ) : (
-              <FaPlus color='#fff' size={14} />
-            )}
-          </SubmitButton>
-        </Form>
-        {formError && (
-          <span style={{ color: 'red' }}>Repositório inválido</span>
-        )}
-
-        <List>
-          {repositories.map((repo) => (
-            <li key={repo.name}>
-              <span>{repo.name}</span>
-              {/* o formato de link normal html (<a>) recarrega a pagina,
-              por isso precisado de um que vem do nosso router
-              <a href={repo.link}>Detalhes</a> */}
-              <Link to={`/repository/${encodeURIComponent(repo.name)}`}>
-                Detalhes
-              </Link>
-            </li>
-          ))}
-        </List>
-      </Container>
-    );
-  }
-}
+      <List>
+        {repositories.map((repo) => (
+          <li key={repo.name}>
+            <span>{repo.name}</span>
+            {/* o formato de link normal html (<a>) recarrega a pagina,
+            por isso precisado de um que vem do nosso router
+            <a href={repo.link}>Detalhes</a> */}
+            <Link to={`/repository/${encodeURIComponent(repo.name)}`}>
+              Detalhes
+            </Link>
+          </li>
+        ))}
+      </List>
+    </Container>
+  );
+};
 
 export default Main;
